d5a7daf1483ad906dda5f4c8c6620b1f902f0bcf
Aahil Lakhani <lakhaniaahil@gmail.com>
2024-11-21 16:00:36 -0500

    Reorganize uMyoData

--- Sources/uMyoBleSdk/uMyoData.swift
+++ Sources/uMyoBleSdk/uMyoData.swift
@@ -24,3 +24,32 @@ public struct uMyoData: Identifiable, Codable {
         self.quaternion = quaternion
     }
 }
+
+public struct Spectrum: Codable {
+    public let sp0: Int
+    public let sp1: Int
+    public let sp2: Int
+    public let sp3: Int
+    
+    public init(sp0: Int, sp1: Int, sp2: Int, sp3: Int) {
+        self.sp0 = sp0
+        self.sp1 = sp1
+        self.sp2 = sp2
+        self.sp3 = sp3
+    }
+}
+
+
+public struct Quaternion: Codable {
+    public let w: Int
+    public let x: Int
+    public let y: Int
+    public let z: Int
+    
+    public init(w: Int, x: Int, y: Int, z: Int) {
+        self.w = w
+        self.x = x
+        self.y = y
+        self.z = z
+    }
+}
